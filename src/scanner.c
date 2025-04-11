6821d676edf223eea38cbf7038892899976dc773
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-04-12 00:01:40 +0200

    16 . 2 A Token at a Time

--- src/scanner.c
+++ src/scanner.c
@@ -6,14 +6,15 @@
 
 typedef struct scanner
 {
-    const char* start;
-    const char* current;
+    const char *start;
+    const char *current;
     int line;
 } Scanner;
 
 Scanner scanner;
 
-void initScanner(const char* source) {
+void initScanner(const char *source)
+{
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
