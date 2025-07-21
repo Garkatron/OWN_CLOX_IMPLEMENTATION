832a584722fd68ef8ca64a25d628f58d7f22e7c3
Garkatron <masitacarg@gmail.com>
2025-07-21 15:29:54 +0100

    25 . 1 Closure Objects

--- src/object.h
+++ src/object.h
@@ -9,14 +9,17 @@
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 
 #define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
 #define AS_CSTRING(value) (AS_STRING(value)->chars)
 #define AS_NATIVE(value) (((ObjNative*)AS_OBJ(value))->function)
+#define AS_CLOSURE(value) ((ObjClosure*)AS_OBJ(value))
 
 typedef enum
 {
+    OBJ_CLOSURE,
     OBJ_FUNCTION,
     OBJ_NATIVE,
     OBJ_STRING,
@@ -37,6 +40,13 @@ struct ObjString
     char *chars;
 };
 
+typedef struct
+{
+    Obj obj;
+    ObjFunction* function;
+} ObjClosure;
+
+
 typedef struct {
     Obj obj;
     int arity;
@@ -50,6 +60,7 @@ typedef struct {
     NativeFn function;
 } ObjNative;
 
+ObjClosure* newClosure(ObjFunction* function);
 ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char *chars, int length);
