83209e33861015639a7c81e0caab205175c585c2
Garkatron <masitacarg@gmail.com>
2025-08-07 23:00:19 +0100

    26 . 7 . 1 Adding to the constant table

--- src/chunk.c
+++ src/chunk.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "chunk.h"
+#include "vm.h"
 
 void initChunk(Chunk *chunk)
 {
@@ -64,7 +65,9 @@ void writeConstant(Chunk *chunk, Value value, int line)
 
 int addConstant(Chunk *chunk, Value value)
 {
+    push(value);
     writeValueArray(&chunk->constants, value);
+    pop();
     return chunk->constants.count - 1;
 }
 int getLine(Chunk *chunk, int index)
