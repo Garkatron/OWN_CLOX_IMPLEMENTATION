2c706579a5048aa50830f328c75dddbfa5a26d23
Garkatron <masitacarg@gmail.com>
2025-08-07 21:07:48 +0100

    26 . 4 . 3 Processing gray objects

--- src/memory.c
+++ src/memory.c
@@ -10,9 +10,9 @@
 
 void *reallocate(void *pointer, size_t oldSize, size_t newSize)
 {
-    #ifdef DEBUG_STRESS_GC
-        collectGarbage();
-    #endif
+#ifdef DEBUG_STRESS_GC
+    collectGarbage();
+#endif
 
     if (newSize == 0)
     {
@@ -26,28 +26,84 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize)
     return result;
 }
 
-void markObject(Obj* object) {
-    if (object == NULL) return;
+void markObject(Obj *object)
+{
+    if (object == NULL)
+        return;
+    if (object->isMarked) return;
 
 #ifdef DEBUG_LOG_GC
-    printf("%p mark ", (void*)object);
+    printf("%p mark ", (void *)object);
     printValue(OBJ_VAL(object));
     printf("\n");
 #endif
 
     object->isMarked = true;
 
-    if (vm.grayCapacity < vm.grayCount + 1) {
+    if (vm.grayCapacity < vm.grayCount + 1)
+    {
         vm.grayCapacity = GROW_CAPACITY(vm.grayCapacity);
-        vm.grayStack = (Obj**)realloc(vm.grayStack, sizeof(Obj*) * vm.grayCapacity);
+        vm.grayStack = (Obj **)realloc(vm.grayStack, sizeof(Obj *) * vm.grayCapacity);
 
-        if (vm.grayStack == NULL) exit(1);
+        if (vm.grayStack == NULL)
+            exit(1);
     }
     vm.grayStack[vm.grayCount++] = object;
 }
 
-void markValue(Value value) {
-    if (IS_OBJ(value)) markValue(AS_OBJ(value));
+void markValue(Value value)
+{
+    if (IS_OBJ(value))
+        markValue(AS_OBJ(value));
+}
+
+static void markArray(ValueArray* array) {
+    for (int i = 0; i < array->count; i++)
+    {
+        markValue(array->values[i]);
+    }
+}
+
+static void blackenObject(Obj *object)
+{
+
+#ifdef DEBUG_LOG_GC
+    printf("%p blacken ", (void*)object);
+    printValue(OBJ_VAL(object));
+    printf("\n");
+#endif
+
+    switch (object->type)
+    {
+    case OBJ_NATIVE:
+    case OBJ_STRING:
+    default:
+        break;
+
+    case OBJ_UPVALUE:
+        markValue(((ObjUpvalue *)object)->closed);
+        break;
+
+    case OBJ_FUNCTION:
+    {
+        ObjFunction *function = (ObjFunction *)object;
+        markObject((Obj *)function->name);
+        markArray(&function->chunk.constants);
+    }
+    
+    case OBJ_CLOSURE: {
+        ObjClosure* closure = (ObjClosure*)object;
+        markObject((Obj*)closure->function);
+        for (int i = 0; i < closure->upvalueCount; i++)
+        {
+            markObject((Obj*)closure->upvalues[i]);
+        }
+        break;
+        
+    }
+
+
+    }
 }
 
 /*
@@ -72,63 +128,76 @@ static void freeObject(Obj *object)
         }
         break;
     }
-    case OBJ_CLOSURE: {
-        ObjClosure* closure = (ObjClosure*)object;
-        FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
+    case OBJ_CLOSURE:
+    {
+        ObjClosure *closure = (ObjClosure *)object;
+        FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
         FREE(ObjClosure, object);
         break;
     }
-    case OBJ_NATIVE: {
+    case OBJ_NATIVE:
+    {
         FREE(ObjNative, object);
         break;
     }
     case OBJ_UPVALUE:
         FREE(ObjUpvalue, object);
         break;
-    case OBJ_FUNCTION: {
+    case OBJ_FUNCTION:
+    {
         /*
-        This switch case is responsible for freeing the ObjFunction itself as well as any other memory it owns. 
+        This switch case is responsible for freeing the ObjFunction itself as well as any other memory it owns.
         Functions own their chunk, so we call Chunk’s destructor-like function.
         */
-        ObjFunction* function = (ObjFunction*)object;
+        ObjFunction *function = (ObjFunction *)object;
         freeChunk(&function->chunk);
         FREE(ObjFunction, object);
         break;
     }
-    
 
     default:
         break;
     }
 }
 
-static void markRoots() {
-    for (Value* slot = vm.stack; slot < vm.stackTop; slot++)
-    {  
+static void traceReferences()
+{
+    while (vm.grayCount > 0)
+    {
+        Obj *object = vm.grayStack[--vm.grayCount];
+        blackenObject(object);
+    }
+}
+
+static void markRoots()
+{
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+    {
         markValue(*slot);
     }
 
     for (int i = 0; i < vm.frameCount; i++)
     {
-        markObject((Obj*)vm.frames[i].closure);
+        markObject((Obj *)vm.frames[i].closure);
     }
 
-    for (ObjUpvalue* upvalue = vm.openUpvalues; upvalue!=NULL; upvalue=upvalue->next)
+    for (ObjUpvalue *upvalue = vm.openUpvalues; upvalue != NULL; upvalue = upvalue->next)
     {
-        markObject((Obj*)upvalue);
+        markObject((Obj *)upvalue);
     }
-    
 
     markTable(&vm.globals);
     markCompilerRoots();
 }
 
-void collectGarbage() {
+void collectGarbage()
+{
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
 #endif
 
     markRoots();
+    traceReferences();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
@@ -137,10 +206,10 @@ void collectGarbage() {
 
 void freeObjects()
 {
-    #ifdef DEBUG_LOG_GC
+#ifdef DEBUG_LOG_GC
     printf("%p free type %d\n");
-    #endif
-    
+#endif
+
     Obj *object = vm.objects;
     while (object != NULL)
     {
