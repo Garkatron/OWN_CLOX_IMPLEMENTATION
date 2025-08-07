4419bdec9cc86b9dd822b5ef2b032944a421b214
Garkatron <masitacarg@gmail.com>
2025-08-07 22:19:25 +0100

    26 . 5 Sweeping Unused Objects

--- src/memory.c
+++ src/memory.c
@@ -169,6 +169,29 @@ static void traceReferences()
     }
 }
 
+static void sweep() {
+    Obj* previous = NULL;
+    Obj* object = vm.objects;
+    while (object != NULL)
+    {
+        if (object->isMarked) {
+            object->isMarked = false;
+            previous = object;
+            object = object->next;
+        } else {
+            Obj* unreached = object;
+            object = object->next;
+            if (previous != NULL) {
+                previous->next = object;
+            } else {
+                vm.objects = object;
+            }
+            freeObject(unreached);
+        }
+    }
+    
+}
+
 static void markRoots()
 {
     for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
@@ -198,6 +221,7 @@ void collectGarbage()
 
     markRoots();
     traceReferences();
+    sweep();
 
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
